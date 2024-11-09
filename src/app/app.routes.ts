28138ec9dbbc02e7e9db4a58f2503406a86365f7
nikibreg <nbregv@yahoo.co.uk>
2024-11-10 02:07:49 +0400

    diverge from signals branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
-import { featureFlagGuard } from './feature-flag.guard';
 import { BoxSelectionObservablesComponent } from './interactive-box-selection/box-selection/box-selection-observables.component';
 
 export const routes: Routes = [
-    { path: '', component: BoxSelectionObservablesComponent, canActivate: [featureFlagGuard] }
+    { path: '', component: BoxSelectionObservablesComponent }
 ];
